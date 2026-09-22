802be6b0b0b000601cc41abcb6df7bc0d2ea2a89
ZachTheMighty <promoted66@gmail.com>
2026-09-22 16:54:09 +0300

    fix: seconds not correctly padded

--- src/utils/seconds_to_ms.js
+++ src/utils/seconds_to_ms.js
@@ -2,6 +2,6 @@ export default function secondToMS(total) {
   const minutes = Math.trunc((total % 3600) / 60)
     .toString()
     .padStart(2, "0");
-  const seconds = (total % 60).toFixed(3).toString().padStart(2, "0");
+  const seconds = (total % 60).toFixed(3).padStart(6, "0");
   return `${minutes}:${seconds}`;
 }
